5c690c824bec1a3ef7fe9e9448b6a60fe5fb8e08
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-05-22 00:21:41 +0200

    refactoring code in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,55 +1,69 @@
-function resetData(){
-    document.getElementById("firstNum").
-    value = "";
-    document.getElementById("secondNum").
-    value = "";
-    document.getElementById("result").
-    innerHTML = "";
+let firstNum = document.getElementById("firstNum");
+let secondNum = document.getElementById("secondNum"); 
+let result = document.getElementById("result");
+let buttonCalc = document.getElementById("start");
+/*TODO add button clear  
+write onSubmit method */
+
+function resetData() {
+    firstNum = "";
+    secondNum = "";
+    result = "";
     }
-  function add(){
-    let num1 = Number(document.getElementById("firstNum").value);
-    let num2 = Number(document.getElementById("secondNum").value);
-    let result = num1 + num2;
+
+function add() {
+    let result = Number(firstNum.value) + Number(secondNum.value);
+    
     return result;
   }
-  function sub(){
-    let num1 = Number(document.getElementById("firstNum").value);
-    let num2 = Number(document.getElementById("secondNum").value);
-    let result = num1 - num2;
-    return result;
+
+function sub() {
+  let result = Number(firstNum.value) - Number(secondNum.value);
+    
+  return result;
   }
-  function mult(){
-    let num1 = Number(document.getElementById("firstNum").value);
-    let num2 = Number(document.getElementById("secondNum").value);
-    let result = num1 * num2;
-    return result;
+  
+function mult() {
+  let result = Number(firstNum.value) * Number(secondNum.value);
+ 
+  return result;
   }
-  function div(){
-    let num1 = Number(document.getElementById("firstNum").value);
-    let num2 = Number(document.getElementById("secondNum").value);
-    let result = num1 / num2;
-    return result;
+  
+function div() {
+  let result = Number(firstNum.value) / Number(secondNum.value);
+    
+  return result;
   }
-  function selectOperator(){
-    let select = document.getElementById("select");
-    let display = select.options[select.selectedIndex].value;
-    return display;
+
+function selectOperator() {
+  let select = document.getElementById("select");
+  let display = select.options[select.selectedIndex].value;
+    
+  return display;
   }
-  function calc(){
-    if(selectOperator() === "+"){
-      document.getElementById("result").
-    innerHTML = add();
-    }
-    else if(selectOperator() === "-"){
-      document.getElementById("result").
-      innerHTML = sub();
-    }
-    else if(selectOperator() === "x"){
-      document.getElementById("result").
-      innerHTML = mult();
-    }
-    else if(selectOperator() === "/"){
-      document.getElementById("result").
-      innerHTML = div();
-    }
+
+function calc() {
+  if (selectOperator() === "+") {
+    document.getElementById("result").innerHTML = add();
   }
+  else if (selectOperator() === "-") {
+    document.getElementById("result").innerHTML = sub();
+  }
+  else if (selectOperator() === "x") {
+    document.getElementById("result").innerHTML = mult();
+  }
+  else if (selectOperator() === "/") {
+    document.getElementById("result").innerHTML = div();
+  }
+  }
+  
+  function onSubmit() {
+
+  }
+
+  function start() {
+    buttonCalc.addEventListener("click", () => calc());
+    console.log(firstNum);
+  }
+
+  start();
